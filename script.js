176ac264cdcb85d7a1dd6d7327eef3489a9b07ac
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-01-22 22:58:56 +0530

    added js file

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 const music = document.querySelector('audio')
 const play = document.getElementById('play');
+const img = document.getElementById('img');
 
 play.addEventListener('click',()=>{
     music.play();
     play.classList.replace("fa-play",'fa-pause')
+    img.classList.add('anime')
 })
