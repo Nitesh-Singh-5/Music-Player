054624a3478e041477b2ddeead663609dafa82e5
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-01-24 21:53:14 +0530

    animation + player

--- script.js
+++ script.js
@@ -3,8 +3,27 @@ const music = document.querySelector('audio')
 const play = document.getElementById('play');
 const img = document.getElementById('img');
 
-play.addEventListener('click',()=>{
+isPlaying = false;
+
+const playMusic=()=>{
+    isPlaying = true;
     music.play();
     play.classList.replace("fa-play",'fa-pause')
     img.classList.add('anime')
+}
+
+// for pause function
+const pauseMusic=()=>{
+    isPlaying = false;
+    music.pause();
+    play.classList.replace("fa-pause",'fa-play')
+    img.classList.remove('anime');
+}
+
+play.addEventListener('click',()=>{
+    if(isPlaying){
+        pauseMusic();      
+    }else{
+        playMusic();
+    }
 })
