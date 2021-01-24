5223cbac29966e00499668d51005a8fdceb5f049
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-01-24 21:37:21 +0530

    Deltee

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const music = document.querySelector('audio')
 const play = document.getElementById('play');
 const img = document.getElementById('img');
 
+play.addEventListener('click',()=>{
+    music.play();
+    play.classList.replace("fa-play",'fa-pause')
+    img.classList.add('anime')
+})
